55bd037a752af3b70a48380bbadd4c17d602e619
priyanka <kacharepriya17@gmail.com>
2020-09-15 15:01:36 +0530

    add dynamic content of notification

--- src/app/common/header/notification/notification.component.ts
+++ src/app/common/header/notification/notification.component.ts
@@ -13,6 +13,16 @@ export class NotificationComponent implements OnInit {
     scrollbarMaxSize: 99,
   };
 
+  notifications = { data: [
+    {
+      type : 'Complaint Resolved!',
+      status:'New', desc:'Lorem ipsum dolor sit amet, consectetur adipisciel Videmusne ut',
+      date:'01 Jan 2020',
+      pay_due : 'Payment Due',
+      day:'Yesterday'
+    }]
+  };
+
   constructor() { }
 
   ngOnInit(): void {
