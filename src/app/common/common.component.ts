24e14e452f79c6f549b49a3cf7020e9e41991706
priyanka <kacharepriya17@gmail.com>
2020-09-29 17:25:37 +0530

    done changes in side nav and breadcrumb

--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -10,6 +10,9 @@ export class CommonComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function(){
+      $('smart360-header, smart360-side-nav, smart360-footer').css('display', 'block');
+    });
   }
 
 }
