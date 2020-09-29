bc1e15fbad9ff136a9a8297a26fe897217e8b4fb
priyanka <kacharepriya17@gmail.com>
2020-09-29 21:38:28 +0530

    add responsiveness in registration

--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -12,6 +12,16 @@ export class CommonComponent implements OnInit {
   ngOnInit(): void {
     $(document).ready(function(){
       $('smart360-header, smart360-side-nav, smart360-footer').css('display', 'block');
+      function myFunction(x) {
+        if (x.matches) {
+          $('.main-container').css('margin-left', '60px');
+        } else {
+          $('.main-container').css('margin-left', '70px');
+        }
+      }
+      var x = window.matchMedia("(max-width: 1599px)")
+      myFunction(x);
+      x.addListener(myFunction);  
     });
   }
 
